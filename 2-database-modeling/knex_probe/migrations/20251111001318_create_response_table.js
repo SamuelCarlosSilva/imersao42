ac4f40935e57c075c0137b3842beaa80eae7d3cc
samuel carlos <samuelcarlos181@gmail.com>
2025-11-10 21:16:14 -0300

    feature: create response table migration

---
+++ 2-database-modeling/knex_probe/migrations/20251111001318_create_response_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('response', function(table) {
+        table.increments('id').primary();
+        table.integer('form_id').unsigned().notNullable()
+          .references('id').inTable('form')
+          .onDelete('SET NULL');
+    
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('response');
+};
